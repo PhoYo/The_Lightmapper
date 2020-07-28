54b9b233b7ba957508b7b0e2b1abdfa384e04e8c
phoyou2 <philipbarker1012@googlemail.com>
2020-07-28 20:38:43 +0100

    lining diffuse node or colour node to bsdf and also deleting multiply node

--- Addon/Operators/gltf.py
+++ Addon/Operators/gltf.py
@@ -36,53 +36,84 @@ class TLM_GltfLightmaps(bpy.types.Operator):
                     # find the nodetree for the material slot
                     nodetree = bpy.data.materials[slot.name].node_tree
 
-                  
+                    principled = nodetree.nodes["Principled BSDF"]
 
                     for node in nodetree.nodes:
+                        print("ALL NODES : " + node.name)
 
-                        if node.type == "MIX_RGB":
-                            print("Mix RGB : " + node.type)
+                        
 
-                            for input in node.inputs:
-                                print(" Mix RGB : outputs : " + input.name)
+                            
+                        if node.name == "Lightmap_Image":
+                            lightmapNode = node
+                            print("node type : " + str(node) + " label : " + node.bl_label + " name : " + node.name)
 
-                                if input.name == "Fac":
-                                    input.default_value = 0
+                            outputNode = nodetree.nodes[0]
 
+                            # if(outputNode.type != "OUTPUT_MATERIAL"):
+                            #     for node in nodetree.nodes:
+                            #         if node.type == "OUTPUT_MATERIAL":
+                            #             outputNode = node
+                            #             break
+                            
+                            #print("out node : " + str(outputNode))
+                            
+                            gltfNode = nodetree.nodes.new(type="ShaderNodeGroup")
+                            gltfNode.node_tree = bpy.data.node_groups["glTF Settings"]
+                            gltfNode.location = ((lightmapNode.location[0] + 600, lightmapNode.location[1]))
+                            gltfNode.name = "gltfNode"
 
-                        if node.type == "TEX_IMAGE":
-                            if node.name == "Lightmap_Image":
-                                lightmapNode = node
-                                print("node type : " + str(node) + " label : " + node.bl_label + " name : " + node.name)
-
-                                outputNode = nodetree.nodes[0]
-
-                                # if(outputNode.type != "OUTPUT_MATERIAL"):
-                                #     for node in nodetree.nodes:
-                                #         if node.type == "OUTPUT_MATERIAL":
-                                #             outputNode = node
-                                #             break
-                                
-                                #print("out node : " + str(outputNode))
-                                
-                                gltfNode = nodetree.nodes.new(type="ShaderNodeGroup")
-                                gltfNode.node_tree = bpy.data.node_groups["glTF Settings"]
-                                gltfNode.location = ((lightmapNode.location[0] + 600, lightmapNode.location[1]))
-                                gltfNode.name = "gltfNode"
-
-                                
-                                for output in lightmapNode.outputs:
-                                    if output.name == "Color":
-                                        #output.default_value = gltfNode.inputs[0].default_value
-                                        print("lightmap output socket : " + output.name)
-                                        
-                                        #link the nodes
-                                        nodetree.links.remove(output.links[0])
-                                        nodetree.links.new(output, gltfNode.inputs[0])
+                            
+                            for output in lightmapNode.outputs:
+                                if output.name == "Color":
+                                    #output.default_value = gltfNode.inputs[0].default_value
+                                    print("lightmap output socket : " + output.name)
                                     
+                                    #link the nodes
+                                    nodetree.links.remove(output.links[0])
+                                    nodetree.links.new(output, gltfNode.inputs[0])
                                 
+                            
 
-                                print("gltf node created : " + gltfNode.name)
+                            print("gltf node created : " + gltfNode.name)
+
+
+                        if node.name == "LM_P":
+                            
+                            LM_Pnode = node
+
+                            for output in LM_Pnode.outputs:
+                                if output.name == "Color":
+                                    nodetree.links.remove(output.links[0])
+                                    nodetree.links.new(output, principled.inputs["Base Color"])
+                        
+
+                        if node.name == "Lightmap_BasecolorNode_A":
+
+                            print("this has base colour")
+
+                            baseColor = node
+
+                            for output in baseColor.outputs:
+                                if output.name == "Color":
+                                    nodetree.links.remove(output.links[0])
+                                    nodetree.links.new(output, principled.inputs["Base Color"])
+
+                    
+                
+                try:
+                #if nodetree.nodes['Lightmap_Multiplication'] is None:
+                #    pass
+                #else :
+                    multiply = nodetree.nodes['Lightmap_Multiplication']
+                    nodetree.nodes.remove( multiply )
+                except:
+                    print("no multiply node")
+                
+                            
+ 
+
+
 
                     # first restore the backup material
                         
